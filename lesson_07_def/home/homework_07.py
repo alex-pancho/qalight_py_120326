b2d7e154cb6e845d7fda838a01467ccabc68c665
Viacheslav Kharchenko <viacheslav.kharchenko@quodfinancial.com>
2026-04-07 23:37:01 +0300

    second_half_of_homework_06

--- lesson_07_def/home/homework_07.py
+++ lesson_07_def/home/homework_07.py
@@ -80,10 +80,83 @@ str2 = "cat"
 print(find_substring(str1, str2)) # поверне -1
 
 # task 7
+# Обробіть список кортежів person_list, що містять ім'я та вік людей,
+# так, щоб отримати словник, де ключі - вікові діапазони (10-19, 20-29 тощо),
+# а значення - списки імен людей, які потрапляють в кожен діапазон.
+# Приклад виводу:
+# {'10-19': ['A'], '20-29': ['B', 'C', 'D'], '30-39': ['E'], '40-49': ['F']}
+# {'10-19': ['A'], '20-29': ['B', 'C', 'D'], '30-39': ['E'], '40-49': ['F']}
+def task_10():
+    person_list = [('Alice', 25), ('Boby', 19), ('Charlie', 32),
+               ('David', 28), ('Emma', 22), ('Frank', 45)]
+    new_dict = {}
+
+    for i in person_list:
+        if 10 <= i[1] <= 19:
+            if '10-19' not in new_dict:
+                new_dict['10-19'] = []
+            new_dict['10-19'].append(i[0])
+
+        if 20 <= i[1] <= 29:
+            if '20-29' not in new_dict:
+                new_dict['20-29'] = []
+            new_dict['20-29'].append(i[0])
+
+        if 30 <= i[1] <= 39:
+            if '30-39' not in new_dict:
+                new_dict['30-39'] = []
+            new_dict['30-39'].append(i[0])
+
+        if 40 <= i[1] <= 49:
+            if '40-49' not in new_dict:
+                new_dict['40-49'] = []
+            new_dict['40-49'].append(i[0])
+    sorted_dict = dict(sorted(new_dict.items()))
+    print(sorted_dict)
 # task 8
+def password_valid_check():
+    passwor_input = input("Введіть пароль")
+    if len(passwor_input) > 8:
+        print("Символів має бути більше 8")
+    else:
+        print("Пароль прийнято")
 # task 9
+def calculator_plus_minus():
+    num1 = float(input("Введіть перше число: "))
+    operation = input("Введіть операцію (+, -, ): ")
+    num2 = float(input("Введіть друге число: "))
+    if operation == "+":
+        print(f"{num1} + {num2} = {num1+num2}")
+    elif operation == "-":
+        print(f"{num1} - {num2} = {num1-num2}")
+    else:
+        print("Ви ввели неправельну операцію")
 # task 10
+def password_valid_check():
+    passwor_input = input("Введіть пароль")
+    if len(passwor_input) > 8:
+        print("Символів має бути більше 8")
+    else:
+        print("Пароль прийнято")
 """  Оберіть будь-які 4 таски з попередніх домашніх робіт та
 перетворіть їх у 4 функції, що отримують значення та повертають результат.
 Обов'язково документуйте функції та дайте зрозумілі імена змінним.
 """
+
+def year_check():
+    year_input = int(input("Введіть рік"))
+    if (year_input % 400 == 0) or (year_input % 4 == 0 and year_input % 100 != 0):
+        print("Рік високосний")
+    else:
+        print("Рік не високосний")
+
+
+
+def golosni_count():
+    text = input("Введіть текст: ").lower()
+    vowels = "аеиіїоуюя"
+    count = 0
+    for ch in text:
+        if ch in vowels:
+            count += 1
+    print(f"Кількість голосних: {count}")
